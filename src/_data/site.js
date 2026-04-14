98cbdafc53ba79366321f36ee1a5f628ebd6adc9
Makeademic <kaplanwizard@gmail.com>
2026-04-14 18:48:38 +0000

    adding images, modifying about, maintainer, menu, site, footer, header

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,10 +1,11 @@
 module.exports = {
-  title: 'KeymapDB',
-  shortDescription: "Database of keymaps for programmable keyboards",
-  description: "KeymapDB is a public and open-source online database for keymaps of programmable keyboards, with a focus on QMK ergonomic mechanical keyboards. You can filter user-submitted keymaps like Miryoku by key count, firmware, keyboard, stagger, languages, OS, home row mods, and more!",
-  keywords: ["QMK", "keyboard", "keymap", "remapping", "database", "open-source", "mechanical keyboards", "ergomech", "keycap", "custom", "customization", "ZMK", "typing", "touchtyping", "macros", "inspiration", "ideas", "QWERTY", "Colemak", "Dvorak", "keyboard layout"],
+  title: 'Shakespeare Census',
+  shortDescription: "Database of all extant copies of all editions of Shakespeare’s works through 1700",
+  description: "The Shakespeare Census attempts to locate and describe all extant copies of all editions of Shakespeare’s works through 1700. We include all items attributed to Shakespeare in print during the period, but not those attributed to him only by modern scholarship. We exclude the Restoration adaptations. Copies attested earlier but currently missing are not included.",
+  keywords: ["Shakespeare", "fragments", "plays"],
   // how many posts you want to show for each page
   paginate: 18,
   // WARNING: This assumes that the pathPrefix is "/"
-  logo: '/assets/svg/logo/light.svg'
+  logo: '/assets/img/favicon.png'
 };
+
